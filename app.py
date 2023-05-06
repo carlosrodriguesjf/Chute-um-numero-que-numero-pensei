f37525d93b9f4d91b392ab5b6979259caa106b7d
Carlos Rodrigues <carlosrodriguesjf@gmail.com>
2023-05-05 23:29:24 -0300

    Criação do PRojeto chute um número

---
+++ app.py
@@ -0,0 +1,38 @@
+# PROJETO 3 - PYTONISTA AUTODIDATA - CHUTE O NÚMERO: QUE NÚMERO EU PENSEI?
+
+import random
+import os
+
+jogo = 's'
+
+input('Aperte enter para começar... ')
+
+while jogo == 's':
+
+    numero_sorteado = random.randint(1,100)
+    print('Foi gerado um número entre 1 e 100... ')
+    print(numero_sorteado)
+
+    while True:
+        numero_chutado = int(input('Chute um número de 1 a 100: '))
+
+        if numero_sorteado == numero_chutado:
+            print('PARABÉNS! VOCÊ CHUTOU O VALOR CERTO: ' + str(numero_sorteado))
+            jogo = input('Jogo encerrado, gostaria de jogar novamente? (s/n)')
+            os.system('cls') or None
+            break
+
+        elif numero_chutado > numero_sorteado:
+            print('Chute um número menor')
+
+        else:
+            print('Chute um número maior')
+
+        
+
+print(50*'-')
+print('Encerrando o jogo...')
+print(50*'-')
+
+
+
